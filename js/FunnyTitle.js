7802c3c47fdbe624d80a81da4faf334d5171b72d
zengnanyang <5226102+zengnanyang@user.noreply.gitee.com>
2021-07-03 21:16:13 +0800

    Site updated: 2021-07-03 21:16:10

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -2,12 +2,12 @@ var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         document.title = '(つェ⊂) 我藏好了哦~ ';
+         document.title = '(つェ⊂) 我在等你哦~ ';
          clearTimeout(titleTime);
      }
      else {
          // $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/guixinchn/image/blog/favicon.png");
-         document.title = '(*´∇｀*) 被你发现啦~ ' + OriginTitle;
+         document.title = '(*´∇｀*) 欢迎回来~ ' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
